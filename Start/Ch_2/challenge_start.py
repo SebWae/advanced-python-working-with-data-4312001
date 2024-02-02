4ce14ff7540e8265da0020a2fb55aba5e6a29154
Sebastian Wærling <swae@itu.dk>
2024-02-02 12:15:45 +0100

    Completed the course

--- Start/Ch_2/challenge_start.py
+++ Start/Ch_2/challenge_start.py
@@ -2,8 +2,17 @@
 # Programming challenge: use advanced data collections on the earthquake data
 
 import json
+from collections import defaultdict
 
 
 # open the data file and load the JSON
-with open("../../30DayQuakes.json", "r") as datafile:
+with open("30DayQuakes.json", "r") as datafile:
     data = json.load(datafile)
+
+eventCounter = defaultdict(int)
+
+for event in data["features"]:
+    eventCounter[event["properties"]["type"]] += 1
+
+for k, v in eventCounter.items():
+    print(f"{k:15}: {v}")
--- Start/Ch_3/deserialize.py
+++ Start/Ch_3/deserialize.py
@@ -9,6 +9,26 @@ import pprint
 result = []
 
 # TODO: open the CSV file for reading
+with open("largequakes.csv", "r") as csvfile:
+    reader = csv.reader(csvfile)
+    sniffer = csv.Sniffer()
+    sample = csvfile.read(1024)
+    csvfile.seek(0)
 
+    # checks if the csvfile has a header, if that is the case, then skip it
+    if sniffer.has_header(sample):
+        next(reader)
+
+    for row in reader:
+        if row != []:
+            # print(row)
+            result.append(
+                {
+                    "place": row[0],
+                    "magnitude": row[1],
+                    "date": row[2],
+                    "link": row[3]
+                }
+            )
 
 pprint.pp(result)
